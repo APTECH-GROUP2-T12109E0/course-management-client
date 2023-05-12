ec4c8f2fa39ffd0b61332fe0323e42d4eddfb939
trucduy10 <trucduy.truong@gmail.com>
2023-05-12 09:42:06 +0700

    blog & detail blog

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const LoginPage = lazy(() => import("./pages/auth/LoginPage.js"));
 const HomePage = lazy(() => import("./pages/HomePage.js"));
 const CoursePage = lazy(() => import("./pages/CoursePage.js"));
 
+const BlogPage = lazy(() => import("./pages/blog/BlogPage.js"));
+const DetailsPage = lazy(() => import("./pages/blog/DetailsPage.js"));
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/courses" element={<CoursePage></CoursePage>}></Route>
 
+        <Route path="/blogs" element={<BlogPage></BlogPage>}></Route>
+        <Route path="/details/:id" element={<DetailsPage></DetailsPage>} />
 
 
         {/* ********* Authentication ********* */}
---
+++ src/pages/blog/BlogPage.js
@@ -0,0 +1,15 @@
+import BlogPage_Carousel from "./BlogPage_Carousel";
+import BlogPage_Select from "./BlogPage_Select";
+
+
+const BlogPage = () => {
+  return (
+    <div>
+      <BlogPage_Carousel />
+      <BlogPage_Select/>
+      
+    </div>
+  );
+};
+
+export default BlogPage;
